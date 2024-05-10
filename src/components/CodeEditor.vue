4df561fe5599c59ca3f37be6bbf9447ba2b85f7c
bobo <570105891@qq.com>
2024-05-10 17:53:50 +0800

    ✨ feat(monaco插件): 增加java语言的支持

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,30 +1,8 @@
 <script setup lang="ts">
 import { onMounted, ref, toRaw, watch } from 'vue';
+// @ts-ignore
+import monaco from '@/plugins/monaco';
 
-import * as monaco from 'monaco-editor'
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
-
-self.MonacoEnvironment = {
-  getWorker(_, label) {
-    if (label === 'json') {
-      return new jsonWorker()
-    }
-    if (label === 'css' || label === 'scss' || label === 'less') {
-      return new cssWorker()
-    }
-    if (label === 'html' || label === 'handlebars' || label === 'razor') {
-      return new htmlWorker()
-    }
-    if (label === 'typescript' || label === 'javascript') {
-      return new tsWorker()
-    }
-    return new editorWorker()
-  }
-}
 
 const props = withDefaults(defineProps<{
   width?: string | number
@@ -40,7 +18,7 @@ const props = withDefaults(defineProps<{
 })
 
 
-const codeValue = defineModel({type: String, default:''});
+const codeValue = defineModel({ type: String, default: '' });
 
 const codeEditor = ref<monaco.editor.IStandaloneCodeEditor>()
 onMounted(() => {
---
+++ src/plugins/monaco/index.ts
@@ -0,0 +1,25 @@
+import * as monaco from 'monaco-editor'
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
+
+import registJava from './javaLang'
+
+self.MonacoEnvironment = {
+  getWorker(_, label) {
+    switch (label) {
+      case 'html':
+      case 'handlebars':
+      case 'razor':
+        return new htmlWorker();
+      case 'typescript':
+      case 'javascript':
+        return new tsWorker();
+      default:
+        return new editorWorker();
+    }
+  }
+}
+registJava(monaco)
+
+export default monaco
